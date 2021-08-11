ed73a070eccf68d8b324183150857430773d301e
kyab <kyossi212@gmail.com>
2021-08-11 11:53:32 +0000

    add launch

---
+++ test/travis_package_make.bash
@@ -0,0 +1,8 @@
+#!/bin/bash -xve
+
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+cd ~/catkin_ws/src
+git clone --depth=1 https://github.com/kyab/pimouse_ros.git
+cd ~/catkin_ws
+
+catkin_make
