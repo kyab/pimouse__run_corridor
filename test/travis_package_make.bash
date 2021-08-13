0fb2f7167ab91d98c8733fc1db3de343aa308d9d
kyab <kyossi212@gmail.com>
2021-08-13 10:22:38 +0000

    Fix reference branch to master

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -2,8 +2,9 @@
 
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 cd ~/catkin_ws/src
-git clone --depth=1 https://github.com/kyab/pimouse_ros.git
+git clone https://github.com/kyab/pimouse_ros.git
 cd pimouse_ros
+git branch -v
 git checkout master
 cd ~/catkin_ws
 
