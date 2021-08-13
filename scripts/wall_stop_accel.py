7b26c80f227730a81cb4553c2f31a5cfe95196be
kyab <kyossi212@gmail.com>
2021-08-13 09:32:40 +0000

    add accel version

---
+++ scripts/wall_stop_accel.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.msg import LightSensorValues
+
+
+class WallStopAccel():
+    def __init__(self):
+        self.cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+
+        self.sensor_values = LightSensorValues()
+        rospy.Subscriber("/lightsensors", LightSensorValues, self.callback)
+
+    def callback(self, messages):
+        self.sensor_values = messages
+
+    def run(self):
+        rospy.loginfo("enter run")
+        rate = rospy.Rate(10)
+        data = Twist() 
+
+        accel = 0.01
+        data.linear.x = 0.0
+        while not rospy.is_shutdown():
+            data.linear.x += accel
+
+            if self.sensor_values.sum_all >= 100: data.linear.x = 0.0
+            elif data.linear.x <= 0.05:          data.linear.x = 0.05
+            elif data.linear.x >= 0.2:          data.linear.x = 0.2
+
+            self.cmd_vel.publish(data)
+            rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node("wall_stop")
+    rospy.wait_for_service("/motor_on")
+    rospy.wait_for_service("/motor_off")
+    rospy.loginfo("waitng for services.......done")
+    rospy.on_shutdown(rospy.ServiceProxy("/motor/off", Trigger).call)
+    rospy.ServiceProxy("/motor_on", Trigger).call()
+    WallStopAccel().run()
+
+
